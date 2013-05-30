fc3e2d6fc3d5cf3843faa1260ffd70141744c0ca
xcloner <xcloner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 18:16:13 +0000

    timezone adjust if it doesn't exist
    git-svn-id: https://plugins.svn.wordpress.org/xcloner-backup-and-restore/tags/3.0.9@720511 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- common.php
+++ common.php
@@ -101,5 +101,6 @@ $_CONFIG['backup_start_path'] 	= $_CONFIG['backup_path'];
 $_CONFIG['backup_store_path'] 	= $_CONFIG['clonerPath'];
 $_CONFIG['temp_dir'] 		= $_CONFIG['backups_dir'];
 
-date_default_timezone_set('America/Los_Angeles');
+if(!date_default_timezone_get())
+	date_default_timezone_set('America/Los_Angeles');
 ?>
