6ad28ca3a03b6fed2a8fcce8e37806864d1613b4
xcloner <xcloner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-25 23:34:36 +0000

    adding 3.1.0 version, Wordpress compatibility check
    git-svn-id: https://plugins.svn.wordpress.org/xcloner-backup-and-restore/tags/3.1.0@865067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.cloner.html.php
+++ admin.cloner.html.php
@@ -158,30 +158,6 @@ document.write(d);
 //-->
 </script></div> </td></tr></table>
 
-<!--XCloner Ads -->
-<br />
-<table width='100%' cellpadding='5' height='100%' class='menu_table'><tr><td>
-<center>
-
-<script type='text/javascript'><!--//<![CDATA[
-   var m3_u = (location.protocol=='https:'?'https://www.xcloner.com/ads/www/delivery/ajs.php':'http://www.xcloner.com/ads/www/delivery/ajs.php');
-   var m3_r = Math.floor(Math.random()*99999999999);
-   if (!document.MAX_used) document.MAX_used = ',';
-   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-   document.write ("?campaignid=3");
-   document.write ('&amp;cb=' + m3_r);
-   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-   document.write ("&amp;loc=" + escape(window.location));
-   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-   if (document.context) document.write ("&context=" + escape(document.context));
-   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-   document.write ("'><\/scr"+"ipt>");
-//]]>--></script><noscript><a href='http://www.xcloner.com/ads/www/delivery/ck.php?n=a6255e1e&amp;cb=1675675575' target='_blank'><img src='http://www.xcloner.com/ads/www/delivery/avw.php?campaignid=3&amp;cb=1675675575&amp;n=a6255e1e' border='0' alt='' /></a></noscript>
-
-</center>
-</td></td></table>
-<!-- END Ads -->
 
 </td><td valign='top' align='left' style="padding-left: 20px;">
 
