a0325c76e153d93c95e7352311866446aa424a5a
xcloner <xcloner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-26 14:24:56 +0000

    adding 3.1.0 version, Wordpress compatibility check
    git-svn-id: https://plugins.svn.wordpress.org/xcloner-backup-and-restore/trunk@865475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xcloner.php
+++ xcloner.php
@@ -96,5 +96,6 @@ function starter_plugin_admin_scripts ($hook) {
 	wp_enqueue_style ('jquery-start-ui-1.8.9.custom.css', plugins_url()."/xcloner-backup-and-restore/css/start/jquery-ui-1.8.9.custom.css", "", "3.1.0");
 	
 }
-add_action('admin_enqueue_scripts', 'starter_plugin_admin_scripts');
+if($_REQUEST["page"] == "xcloner_show")
+	add_action('admin_enqueue_scripts', 'starter_plugin_admin_scripts');
 
